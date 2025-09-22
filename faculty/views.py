1a236c987da0dad80b5330d2e48552272853ea64
deffin05 <dm.melnyk05@gmail.com>
2025-09-22 18:51:59 +0300

    Implement departments/ page

--- faculty/views.py
+++ faculty/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from faculty.models import Department, Major
 # Create your views here.
 
 def index(request):
     return render(request, 'faculty/index.html')
 
 def get_departments(request):
-    return render(request, 'faculty/departments.html')
+    departments = Department.objects.all()
+    return render(request, 'faculty/departments.html', {'departments': departments})
 
 def get_department(request, department_id):
     return render(request, 'faculty/department.html')
