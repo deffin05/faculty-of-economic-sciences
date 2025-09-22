40d0293164df7d2ecd684dc19745dd8db06254b2
deffin05 <dm.melnyk05@gmail.com>
2025-09-22 17:15:32 +0300

    Register models in admin panel

--- faculty/admin.py
+++ faculty/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from faculty.models import Subject, Department, Professor, Major
+
+
 # Register your models here.
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'head']
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'position', 'department']
+
+class MajorAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'coordinator_name', 'department']
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Professor, ProfessorAdmin)
+admin.site.register(Major, MajorAdmin)
