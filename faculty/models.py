19c15e9ac20b5b621c10eb4b1d88ec62ba73714e
deffin05 <dm.melnyk05@gmail.com>
2025-09-23 13:43:55 +0300

    Tweak models to better suit the task

--- faculty/models.py
+++ faculty/models.py
@@ -6,18 +6,18 @@ class Professor(models.Model):
     name = models.CharField(max_length=200)
     position = models.CharField(max_length=100)
     degree = models.CharField(max_length=100)
-    department = models.ForeignKey('Department', on_delete=models.SET_NULL, blank=True, null=True, related_name='professors')
+    department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True, related_name='professors')
 
 class Department(models.Model):
-    name = models.CharField(max_length=100)
-    head = models.ForeignKey('Professor', on_delete=models.SET_NULL, blank=True, null=True, related_name='departments')
+    name = models.CharField(max_length=200)
+    head = models.CharField(max_length=200, null=True)
 
 class Major(models.Model):
-    name = models.CharField(max_length=100)
-    code = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=10)
     description = models.TextField()
-    coordinator_name = models.CharField(max_length=100)
-    coordinator_contact = models.CharField(max_length=100)
+    coordinator_name = models.CharField(max_length=200)
+    coordinator_contact = models.CharField(max_length=50)
     department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True)
 
     def get_short_description(self):
@@ -28,4 +28,4 @@ class Major(models.Model):
 
 class Subject(models.Model):
     major = models.ForeignKey('Major', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
